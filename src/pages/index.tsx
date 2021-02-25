08dcc1fcaf3865e798a650d3143dca09448c1506
Jefferson <jeffersonsantos.a@gmail.com>
2021-02-24 21:24:37 -0300

    Terceiro dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Head from 'next/head'
+
 import { CompletedChallenges } from "../components/CompletedChallenges"
 import { Countdown } from "../components/Countdown"
 import { ExperienceBar } from "../components/ExperienceBar"
 import { Profile } from "../components/Profile"
-
-import Head from 'next/head'
+import { ChallengerBox } from "../components/ChallengerBox"
 
 import styles from "../styles/pages/Home.module.css"
 
@@ -21,7 +22,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengerBox />
+        </div>
       </section>
     </div>
   )
