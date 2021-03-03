bdb3a08ec13469ed2466d725fb057a4e0ad3e415
Jefferson <jeffersonsantos.a@gmail.com>
2021-03-03 18:41:54 -0300

    Começando a tabela

--- src/pages/learderboard.tsx
+++ src/pages/learderboard.tsx
@@ -1,6 +1,17 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import styles from '../styles/pages/Learderboard.module.css'
 
 import { Layout } from "../components/Layout"
+import { rmdirSync } from 'fs'
+
 
 export default function Learderboard() {
   const users = [
@@ -18,6 +29,28 @@ export default function Learderboard() {
     <Layout>
       <div className={styles.container}>
         <h1>Learderboard</h1>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>POSIÇÃO</TableCell>
+                <TableCell align="left">USUÁRIO</TableCell>
+                <TableCell align="left">DESAFIOS</TableCell>
+                <TableCell align="left">EXPERIÊNCIA</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map((row) => (
+                <TableRow key={row.user}>
+                  <TableCell component="th" scope="row">
+                    {row.position}
+                  </TableCell>
+                  <TableCell align="left">{row.user} Level {row.user_level}</TableCell>
+                  <TableCell align="left">{row.challenges} completados</TableCell>
+                  <TableCell align="left">{row.exp} xp</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
       </div>
     </Layout>
   )
