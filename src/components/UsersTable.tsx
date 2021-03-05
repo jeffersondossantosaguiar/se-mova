2dc920331c10814cc721a0ba875121adeaeaca5b
Jefferson <jeffersonsantos.a@gmail.com>
2021-03-04 22:50:45 -0300

    Leaderboard

---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,76 @@
+import styles from '../styles/components/UsersTable.module.css'
+
+const users = [
+  {
+    position: 1,
+    name: "Jefferson Aguiar",
+    level: 59,
+    avatar_url: "https://github.com/jeffersondossantosaguiar.png",
+    challenges: 130,
+    exp: 159000
+  },
+  {
+    position: 2,
+    name: "Joselito",
+    level: 19,
+    avatar_url: "https://i1.sndcdn.com/artworks-000123960697-yayeil-t500x500.jpg",
+    challenges: 100,
+    exp: 59000
+  },
+  {
+    position: 3,
+    name: "Ricardo Milos",
+    level: 12,
+    avatar_url: "https://pbs.twimg.com/profile_images/1181974658230435840/N0rcgveK.jpg",
+    challenges: 13,
+    exp: 9000
+  }
+]
+
+const columns = [
+  { title: "POSIÇÃO" },
+  { title: "USUÁRIO" },
+  { title: "DESAFIOS" },
+  { title: "EXPERIÊNCIA" }
+]
+
+export function UsersTable() {
+  return (
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <tr className={styles.tr_header}>
+          {columns.map(column => (
+            <th className={`${column.title === "USUÁRIO" ? styles.th_user : styles.th}`}>
+              {column.title}
+            </th>
+          ))}
+        </tr>
+        {users.map(user => (
+          <tr className={styles.tr_data}>
+            <td className={styles.td_position}>
+              {user.position}
+            </td>
+            <td className={styles.td_user}>
+              <div>
+                <img className={styles.avatar} src={user.avatar_url} />
+              </div>
+              <div className={styles.name_level}>
+                <strong className={styles.strong}>{user.name}</strong>
+                <p className={styles.p}>
+                  <img className={styles.avatar_img} src='icons/level.svg' />
+                   Level {user.level}</p>
+              </div>
+
+            </td>
+            <td className={styles.td_challenges}>
+              <span className={styles.blue}>{user.challenges}</span> completados
+            </td>
+            <td className={styles.td_exp}>
+              <span className={styles.blue}>{user.exp}</span> xp
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
+}
--- src/pages/learderboard.tsx
+++ src/pages/learderboard.tsx
@@ -1,56 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
 import styles from '../styles/pages/Learderboard.module.css'
 
 import { Layout } from "../components/Layout"
-import { rmdirSync } from 'fs'
-
+import { UsersTable } from '../components/UsersTable'
 
 export default function Learderboard() {
-  const users = [
-    {
-      position: 1,
-      user: "Jefferson",
-      user_level: 59,
-      user_avatar: "https://github.com/jeffersondossantosaguiar.png",
-      challenges: 130,
-      exp: 159000
-    }
-  ]
-
   return (
     <Layout>
       <div className={styles.container}>
         <h1>Learderboard</h1>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>POSIÇÃO</TableCell>
-                <TableCell align="left">USUÁRIO</TableCell>
-                <TableCell align="left">DESAFIOS</TableCell>
-                <TableCell align="left">EXPERIÊNCIA</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {users.map((row) => (
-                <TableRow key={row.user}>
-                  <TableCell component="th" scope="row">
-                    {row.position}
-                  </TableCell>
-                  <TableCell align="left">{row.user} Level {row.user_level}</TableCell>
-                  <TableCell align="left">{row.challenges} completados</TableCell>
-                  <TableCell align="left">{row.exp} xp</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+        <UsersTable />
       </div>
     </Layout>
   )
