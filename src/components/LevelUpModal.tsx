64f2f10b3736aa72bf5bed8fa8b0fac2de070370
Jefferson <jeffersonsantos.a@gmail.com>
2021-03-08 20:08:19 -0300

    Adicionando botao twitter

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
+import { FaTwitter } from 'react-icons/fa'
+
 import styles from '../styles/components/LevelUpModal.module.css'
 
 export function LevelUpModal() {
-  const {level, closeLevelUpModal} = useContext(ChallengesContext)
+  const { level, closeLevelUpModal } = useContext(ChallengesContext)
 
   return (
     <div className={styles.overlay}>
@@ -14,9 +16,13 @@ export function LevelUpModal() {
         <p>Você alcançou um novo level.</p>
 
         <button type="button" onClick={closeLevelUpModal}>
-          <img src="/icons/close.svg" alt="Fechar modal"/>
+          <img src="/icons/close.svg" alt="Fechar modal" />
         </button>
       </div>
+      <button className={styles.twitterButton}>
+        <p>Compartilhar no Twitter</p>
+        <FaTwitter className={styles.twitterIcon} />
+      </button>
     </div>
   )
 }
